b11de9813ebc5eae1bb7c405f75fe1d244d5c768
Robert Xie <98110427+xieshihua@users.noreply.github.com>
2024-10-29 14:31:18 -0700

    Added port range support

--- net-tools/Query_Ports.ps1
+++ net-tools/Query_Ports.ps1
@@ -1,14 +1,14 @@
 # Name: Query_Ports.ps1
 # Purpose: Query ports for accessibility.
-# Usage: Query_Ports.ps1 -ConfigFile [configfile]
+# Usage: Query_Ports.ps1 -ConfigFile [configfile] -LogFile [logFile]
 # Comments:
-# - The ConfigFile defaults to config.json, but you may specify a different config file by -ConfigFile parameter.
+# - ConfigFile: Optional, defaults to config.json, but you may specify a different config file by -ConfigFile parameter.
 #   Please update the info in the config file before running the script.
-# - It creates a Log folder if it is not exist and put a log file there.
+# - LogFile: Optional, defaults to the Logs folder. The Logs folder is created if it is missing.
 # Content of config.json:
 <#
 {   
-    "ports": ["1002", "1005", "1521"],
+    "ports": ["1002", "1005", "1100-1113", "1521"],
     "servers": ["server_1",
         "server_2",
         "server_3"
@@ -20,14 +20,18 @@
 # Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 param (
-    $ConfigFile = (Get-Location).Path + "\config.json"
+    $ConfigFile = (Get-Location).Path + "\config.json",
+    $LogFile = "defaultLog.log"
 )
 
 $wkPath=(Get-Location).Path
-$logPath="$wkPath\Logs"
-if (-not(Test-Path -Path $logPath)) {$null = New-Item -ItemType Directory -Path $logPath -Force}
-$logFile="$logPath\Query_Ports_" + (Get-Date -Format "yyyy-MM-dd-HHmm") + ".log"
-Start-Transcript -Path $logFile -Append
+if ($LogFile -eq "defaultLog.log") {
+    $logPath="$wkPath\Logs"
+    if (-not(Test-Path -Path $logPath)) {$null = New-Item -ItemType Directory -Path $logPath -Force}
+    $LogFile="$logPath\Query_Ports_" + (Get-Date -Format "yyyy-MM-dd-HHmm") + ".log"
+}
+
+Start-Transcript -Path $LogFile -Append
 Write-Host "**********************`n"
 Write-Host "`n"
 
@@ -42,7 +46,14 @@ for ($i=0; $i -lt $servers.Count; $i++) {
         #& $cmd -n $servers[$i] -e $ports[$j]
         # Option 2. telnet is not supported in Powershell. telnet syntax: telnet computerName port
         # Option 3. Use Test-NetConnection
-        Test-NetConnection -ComputerName $servers[$i] -Port $ports[$j]
+        $arPorts = $ports[$j].Split("-")
+        Write-Host "ports[$j]: $ports[$j]"
+        Write-Host "arPorts[0]: " + $arPorts[0]
+        Write-Host "arPorts[1]: " + $arPorts[1]
+        for ($k=[int]$arPorts[0]; $k -lt [int]$arPorts[$arPorts.Count - 1]+1; $k++) {
+            Write-Host "k: $k"
+            Test-NetConnection -ComputerName $servers[$i] -Port $k
+        }
     }
 }
 
