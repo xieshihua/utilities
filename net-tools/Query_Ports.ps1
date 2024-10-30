7429827e96566ce434e917785e08a3a7e3d9252e
Robert Xie <98110427+xieshihua@users.noreply.github.com>
2024-10-30 10:17:48 -0700

    Added port override support

--- net-tools/Query_Ports.ps1
+++ net-tools/Query_Ports.ps1
@@ -2,16 +2,23 @@
 # Purpose: Query ports for accessibility.
 # Usage: Query_Ports.ps1 -ConfigFile [configfile] -LogFile [logFile]
 # Comments:
-# - ConfigFile: Optional, defaults to config.json, but you may specify a different config file by -ConfigFile parameter.
+# - ConfigFile: Optional, defaults to serverPortList.json, but you may specify a different config file by -ConfigFile parameter.
 #   Please update the info in the config file before running the script.
 # - LogFile: Optional, defaults to the Logs folder. The Logs folder is created if it is missing.
-# Content of config.json:
+# Content of serverPortList.json:
 <#
 {   
-    "ports": ["1002", "1005", "1100-1113", "1521"],
-    "servers": ["server_1",
-        "server_2",
-        "server_3"
+    "defaultPorts": ["1002", "1005", "1100-1113", "1521"],
+    "comments": [
+        "Ports can be a specific port (eg. 1007) or a range of ports (eg. 1100-1103)",
+        "defaultPorts are used if a specific server's ports field is blank []",
+        "Specify ports for a specific server to override the default ports."
+    ],
+    "Servers": [
+        {"name": "server_1.domain_1", "ports": ["5438"]},
+        {"name": "www.some_site.com", "ports": []},
+        {"name": "some_host", "ports": []},
+        {"name": "server_2.domain_1", "ports": ["1007", "2200-2203", "5521"]}
     ]
 }
 #>
@@ -20,7 +27,7 @@
 # Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 param (
-    $ConfigFile = (Get-Location).Path + "\config.json",
+    $ConfigFile = (Get-Location).Path + "\serverPortList.json",
     $LogFile = "defaultLog.log"
 )
 
@@ -32,14 +39,29 @@ if ($LogFile -eq "defaultLog.log") {
 }
 
 Start-Transcript -Path $LogFile -Append
-Write-Host "**********************`n"
+Write-Host "**************************************`n"
 Write-Host "`n"
 
 $QueryConfig=Get-Content -Path $ConfigFile | ConvertFrom-json
-$ports=$QueryConfig.ports
+$defaultPorts=$QueryConfig.defaultPorts
 $servers=$QueryConfig.servers
 
+Write-Host "defaultPorts: " $defaultPorts
+Write-Host "======================================"
+Write-Host
+
 for ($i=0; $i -lt $servers.Count; $i++) {
+    $svrPorts=$servers[$i].ports
+    Write-Host $servers[$i].name " :: ports: $svrPorts"
+
+    if ($servers[$i].ports -gt 0) {
+        $ports = $servers[$i].ports
+    } else {
+        $ports = $defaultPorts
+    }
+    Write-Host "Actual ports used for scanning: $ports"
+    Write-Host "-------------------------------------------"
+
     for ($j=0; $j -lt $ports.Count; $j++) {
         # Option 1. Use portqry.exe
         #$cmd=".\portqry.exe"
@@ -47,12 +69,10 @@ for ($i=0; $i -lt $servers.Count; $i++) {
         # Option 2. telnet is not supported in Powershell. telnet syntax: telnet computerName port
         # Option 3. Use Test-NetConnection
         $arPorts = $ports[$j].Split("-")
-        Write-Host "ports[$j]: $ports[$j]"
-        Write-Host "arPorts[0]: " + $arPorts[0]
-        Write-Host "arPorts[1]: " + $arPorts[1]
+        #Write-Host "arPorts: " + $arPorts
         for ($k=[int]$arPorts[0]; $k -lt [int]$arPorts[$arPorts.Count - 1]+1; $k++) {
-            Write-Host "k: $k"
-            Test-NetConnection -ComputerName $servers[$i] -Port $k
+            #Write-Host "Current port: $k"
+            Test-NetConnection -ComputerName $servers[$i].name -Port $k
         }
     }
 }
