66246e01d25a6557c022819959bcab5327121d6c
Robert Xie <98110427+xieshihua@users.noreply.github.com>
2026-08-24 16:12:28 -0700

    Update save_permission_batch.cmd

--- cmd/save_permission_batch.cmd
+++ cmd/save_permission_batch.cmd
@@ -1,44 +1,51 @@
 echo off
 setlocal enabledelayedexpansion
 
-set BlockDivider0="*********************************************************************"
-set BlockDivider1="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-set Heading_Line0="####################"
-set Heading_Line1="===================="
-set Heading_Line2="--------------------"
+set Block_Divider_0="*********************************************************************"
+set Block_Divider_1="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+set Heading_Line_0="####################"
+set Heading_Line_1="===================="
+set Heading_Line_2="--------------------"
 
 set Name="save_permission_batch.cmd"
-set Purpose0="Batch backup permissions grouped by sub directories with exclusion list support."
-set Purpose1="- Creates top level sub directories of the [source_directory] in the [destination_directory]."
-set Purpose2="- And then batch backs up permission of subsequent sub directories to text files in coresponding sub directories in the [destination_directory]."
-REM set Usage0="."
-set Usage0="save_permission_batch.cmd [/?] [/T] source_directory destination_directory"
-rem Below are optional Help items. Comment out any item that you do not use.
+
+set Optional_Info_Sections=Usage,Example,Remark
+set /A Max_Section_Items=7
+
+set Purpose0="Batch backup permissions grouped by sub folders with exclusion list support."
+set Purpose1="- Create the [destination_path]."
+set Purpose2="- Creates top level sub folders of the [source_path] in the [destination_path]."
+set Purpose3="- And then batch backs up permission of subsequent sub folders to text files in coresponding sub folders in the [destination_path]."
+set Purpose4="- Folders specified in [Exclusion_List] variable are excluded."
+set Purpose5="- An additional folder can be added to the [Exclusion_List] through the argument [/EX:path] at the runtime."
+
+set Usage0="save_permission_batch.cmd [/?] [/T] [/EX:path] source_path destination_path"
 set Usage1="."
-set Usage2="  [/?]                   Optional. Show Help."
-set Usage3="  [/T]                   Optional. Test run or dry run without writing."
-set Usage4="  source_directory       Permissions of all sub directories to be backed up."
-set Usage5="  destination_directory  Permissions will be backed up to this directory."
-set Usage6="."
-set Example0="The following command displays the process of saving permissions of sub directories of D:\ to C:\temp\D_Permissions without create folder or file."
+set Usage2="  [/?]              Optional. Show Help."
+set Usage3="  [/T]              Optional. Test run or dry run without writing."
+set Usage4="  [/EX:path]        Optional. Additional excluded path."
+set Usage5="  source_path       Permissions of all sub folders to be backed up."
+set Usage6="  destination_path  Permissions will be backed up to this path."
+set Usage7="."
+
+set Example0="The following command displays the process of saving permissions of sub folders of D:\, excluding folders specified by [Exclusion_List] and D:\temp specified by [/EX:temp], to C:\temp\D_Permissions without create folder or file."
 set Example1="."
-set Example2="  C:\dvlp>save_permission_batch D: C:\temp\D_Permissions /t"
+set Example2="  C:\dvlp>save_permission_batch D: C:\temp\D_Permissions /EX:temp /t"
 set Example3="."
+
 set Remark0="Remarks:"
 set Remark1="1. This script calls save_permission.cmd. You must update Path_save_permission variable."
 set Remark2="2. Alternatively, you may add the path of save_permission.cmd to the Path environment variable." 
-set Remark3="3. Set the Exclusion_List variable to exclude sub directories you want to escape."
+set Remark3="3. Set the Exclusion_List variable to exclude sub folders you want to escape."
 rem set Remark1="Set the EffArg_Required to the number of mandatory arguments."
-rem set Reference0="A thorough reference to Windows CMD commands: https://ss64.com/nt/"
-set Head_Sections=Usage,Example,Remark
-set Max_Help_Items=0,1,2,3,4,5,6
 
-echo %BlockDivider0:"=%
+rem set Reference0="A thorough reference to Windows CMD commands: https://ss64.com/nt/"
+
+echo %Block_Divider_0:"=%
 echo %Name:"=%:
 call :MSG_Lines "Purpose"
-echo %BlockDivider0:"=%
+echo %Block_Divider_0:"=%
 
-REM 'shift' will process all the argument.
 set /A Arg_Count=0
 set /A Effective_Args=0
 set /A EffArg_Required=2
@@ -48,13 +55,23 @@ set Path_save_permission=C:\Dvlp\Library\source\CMD
 set Exclusion_List="$Recycle.Bin","Recovery","System Volume Information"
 set Is_in_List=FALSE
 
+echo %Block_Divider_1:"=%
+echo [All arguments]: %*
+
+REM 'shift' will process all the argument.
 :arg_loop
 if "%~1"=="" goto end_arg_loop
 	set /a Arg_Count+=1
 	set Is_Effective=TRUE
+	set tmp_arg=%~1
 	rem echo argument !Arg_Count!: %~1
 	rem /I          Do a case Insensitive string comparison.
-	if /I "%~1" == "/t" (
+	if /I "!tmp_arg:~0,3!" == "/EX" (
+		rem echo [tmp_arg]: !tmp_arg!
+		set Path_to_Exclude=!tmp_arg:~4!
+		set Is_Effective=FALSE
+	)
+	if /I "%~1" == "/T" (
 		set Is_Test=TRUE
 		set Is_Effective=FALSE
 	)
@@ -67,10 +84,10 @@ if "%~1"=="" goto end_arg_loop
 		set /A Effective_Args+=1
 		rem echo Effective Args: !Effective_Args!
 		if !Effective_Args! == 1 (
-			set source_directory=%~1
+			set source_path=%~1
 		)
 		if !Effective_Args! == 2 (
-			set destination_directory=%~1
+			set destination_path=%~1
 		)
 		if !Effective_Args! GTR %EffArg_Required% (
 			if "!unexpected_args!" GTR "" (
@@ -98,36 +115,55 @@ if not !Is_Help!==TRUE (
 )
 
 if !Is_Help!==TRUE (
+	echo %Block_Divider_1:"=%
 	call :MSG_Help
+	endlocal
 	exit /b 0
 )
 
-rem Trim ending "\".
-if "!source_directory:~-1!" equ "\" (
-	set source_directory=!source_directory:~0,-1!
-)
-if "!destination_directory:~-1!" equ "\" (
-	set destination_directory=!destination_directory:~0,-1!
+if defined Path_to_Exclude (
+	rem echo [Path_to_Exclude]: !Path_to_Exclude!
+	set Exclusion_List=%Exclusion_List%,"!Path_to_Exclude!"
+	echo [Effective Exclusion List]: !Exclusion_List!
 )
 
-echo Batch source root: !source_directory!
-echo Batch destination root: !destination_directory!
+rem Trim ending "\".
+if "!source_path:~-1!" equ "\" (
+	set source_path=!source_path:~0,-1!
+)
+if "!destination_path:~-1!" equ "\" (
+	set destination_path=!destination_path:~0,-1!
+)
+
+echo [Batch source root]: !source_path!
+echo [Batch destination root]: !destination_path!
+echo %Block_Divider_1:"=%
+echo.
+
+echo Create destination root: [!destination_path!]
+if !Is_Test! == TRUE (
+	echo mkdir !destination_path!
+) else (
+	mkdir !destination_path!
+)
+echo.
+
 
 set Time_Started=%date%:%time%
-echo [!source_directory!] Time started: %Time_Started%
+echo [!source_path!] Time started: %Time_Started%
 
 rem *** Batch Processing ***
-for /f "tokens=*" %%a in ('dir /a:d
- /b !source_directory!\') do (
+for /f "tokens=*" %%a in ('dir /a:d /b !source_path!\') do (
+	set src=!source_path!\%%a
+	echo.
+	echo %Heading_Line_0:"=% Processing Folder: [!src!] %Heading_Line_0:"=%
 	call :Test_Membership "%%a"
 	if !Is_in_List! == TRUE (
 		echo [%%a] is excluded.
-	) else (
-		set src=!source_directory!\%%a
-		set dst=!destination_directory!\%%a
-		rem stepwise is easier to read and maintain then blockwise.
 		echo.
-		echo %Heading_Line0:"=% Processing directory: [!src!] %Heading_Line0:"=%
+	) else (
+		set dst=!destination_path!\%%a
+		rem stepwise is easier to read and maintain then blockwise.
 
 		set SubTime_Started=!date!:!time!
 		echo [!src!] time started: !SubTime_Started!
@@ -139,14 +175,16 @@ for /f "tokens=*" %%a in ('dir /a:d
 			mkdir !dst!
 		)
 		echo.
-		echo %Heading_Line1:"=% Save root level permissions %Heading_Line1:"=%
+		echo %Heading_Line_1:"=% Save root level permissions %Heading_Line_1:"=%
+		rem Insert "." infront of the file name to bring it up to the top.
+		set root_file=".%%~a"
 		if !Is_Test! == TRUE (
-			call "%Path_save_permission%\save_permission.cmd" "!src!" "!dst!" "%%a" /t
+			call "%Path_save_permission%\save_permission.cmd" "!src!" "!dst!" !root_file! /t
 		) else (
-			call "%Path_save_permission%\save_permission.cmd" "!src!" "!dst!" "%%a"
+			call "%Path_save_permission%\save_permission.cmd" "!src!" "!dst!" !root_file!
 		)
 		echo.
-		echo %Heading_Line2:"=% Save sub directory Permissions %Heading_Line2:"=%
+		echo %Heading_Line_2:"=% Save Sub Folder Permissions %Heading_Line_2:"=%
 		echo Root: [!src!]
 		if !Is_Test! == TRUE (
 			forfiles /p "!src!" /s /c "cmd /C if @isdir == TRUE (call "%Path_save_permission%\save_permission.cmd" "@path" "!dst!" "@relpath" /t) else (echo Escape file: @path)"
@@ -162,10 +200,10 @@ for /f "tokens=*" %%a in ('dir /a:d
 echo off
 
 echo.
-echo %Heading_Line0:"=%***  Total Time Elapsed  ****%Heading_Line0:"=%
-echo [!source_directory!] Time started: %Time_Started%
-echo [!source_directory!] Time finished: !date!:!time!
-echo %BlockDivider1:"=%
+echo %Heading_Line_0:"=%***  Total Time Elapsed  ****%Heading_Line_0:"=%
+echo [!source_path!] Time started: %Time_Started%
+echo [!source_path!] Time finished: !date!:!time!
+echo %Block_Divider_1:"=%
 
 endlocal
 echo on
@@ -174,7 +212,9 @@ exit /B %errorlevel%
 :Test_Membership
 	set Is_in_List=FALSE
 
-	for %%b in (%Exclusion_List%) do (
+	for %%b in (!Exclusion_List!) do (
+		rem echo [exclude dir]: %%~b
+		rem echo [passed in dir]: %~1
 		if /I "%%~b" equ "%~1" (
 			set Is_in_List=TRUE
 			Exit /B 0
@@ -192,16 +232,16 @@ Exit /B 0
 
 :MSG_Help
 	echo.
-	REM echo %BlockDivider1:"=%
+	REM echo %Block_Divider_1:"=%
 	REM echo Name: %Name:"=% 
-	for %%a in (%Head_Sections%) do (
+	for %%a in (%Optional_Info_Sections%) do (
 		call :MSG_Lines "%%~a"
 	)
-	REM echo %BlockDivider1:"=%
+	REM echo %Block_Divider_1:"=%
 Exit /B 0
 
 :MSG_Lines
-	for %%i in (%Max_Help_Items%) do (
+	for /L %%i in (0,1,%Max_Section_Items%) do (
 		if defined %~1%%i (
 			set msg=!%~1%%i!
 			if !msg! equ "." (
